b95c7716936264fa0f746821a218016d33f3b01d
Hernan <hernanmoralesprog@gmail.com>
2024-08-20 17:49:13 -0300

    el archivo ContratoDao tiene obj de conexion a BD

---
+++ Datos/ContratoDao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABMEpec.Datos
+{
+    internal class ContratoDao
+    {
+        AccesoDatos oBD;
+
+        public ContratoDao()
+        {
+            oBD = new AccesoDatos();
+        }
+
+        public DataTable ConsultarTabla(string nombreTabla)
+        {
+            return oBD.ConsultarTabla(nombreTabla);
+        }
+
+        public int ActualizarBD(string consultaSQL, List<Parametro> lParametros)
+        {
+            return oBD.ActualizarBD(consultaSQL, lParametros);
+        }
+
+    }
+}
---
+++ Negocio/ServicioContrato.cs
@@ -0,0 +1,19 @@
+﻿using ABMEpec.Datos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABMEpec.Negocio
+{
+   
+    internal class ServicioContrato
+    {
+       
+
+
+
+    }
+}
